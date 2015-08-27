ad7a0429b2ce6e7bb09d85d40184d0c12fc262b0
Alexander Miehe <alexander.miehe@gmail.com>
2015-08-27 15:18:05 +0200

    * add get single video object call

--- src/Video/VideoService.php
+++ src/Video/VideoService.php
@@ -3,7 +3,6 @@
 namespace Mi\VideoManagerPro\SDK\Video;
 
 use GuzzleHttp\Command\Guzzle\GuzzleClient;
-use Mi\VideoManagerPro\SDK\Model\VideoList;
 
 /**
  * @author Alexander Miehe <alexander.miehe@movingimage.com>
@@ -15,10 +14,21 @@ class VideoService extends GuzzleClient
     /**
      * @param integer $videoManagerId
      *
-     * @return VideoList
+     * @return \Mi\VideoManagerPro\SDK\Model\VideoList
      */
     public function getAllVideos($videoManagerId)
     {
         return $this->execute($this->getCommand('list', ['videoManagerId' => $videoManagerId,'limit' => 99999999]));
+    }
+
+    /**
+     * @param integer $videoManagerId
+     * @param integer $videoId
+     *
+     * @return \Mi\VideoManagerPro\SDK\Model\Video
+     */
+    public function getVideo($videoManagerId, $videoId)
+    {
+        return $this->execute($this->getCommand('get', ['videoManagerId' => $videoManagerId,'videoId' => $videoId]));
     }
 }
