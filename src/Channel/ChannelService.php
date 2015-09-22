4294446d8ae5c9875dcd12b7b1992fbcb9c86d22
hibulix <patrick.ndongo@movingimage.com>
2015-09-22 14:31:01 +0200

    stat-VMPRO-49

---
+++ src/Channel/ChannelService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mi\VideoManagerPro\SDK\Channel;
+
+use GuzzleHttp\Command\Guzzle\GuzzleClient;
+
+/**
+ * @author Alexander Miehe <alexander.miehe@movingimage.com>
+ *
+ * @codeCoverageIgnore
+ */
+class ChannelService extends GuzzleClient
+{
+    /**
+     * @param integer $videoManagerId
+     *
+     * @return \Mi\VideoManagerPro\SDK\Model\ChannelTree
+     */
+    public function getAllChannels($videoManagerId)
+    {
+        return $this->execute($this->getCommand('list', ['videoManagerId' => $videoManagerId]));
+    }
+
+}
